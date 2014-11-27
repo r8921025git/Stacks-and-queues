516d19ebc4cf221f95de7ea9f0b9e64f8dd3ae89
r8921025git <r8921025+git@yahoo.com.tw>
2014-11-27 06:59:35 -0500

    Update stack with MAX API.cpp

--- stack with MAX API.cpp
+++ stack with MAX API.cpp
@@ -6,7 +6,7 @@
 #include <stdexcept>
 #include <string>
 #include <utility>
-
+#include <stack>
 
 using namespace std;
 
@@ -15,19 +15,41 @@ class Stack {
 public:
     void Push(int x);
     int Pop();
-    int Max();
+    const int Max() const;
+    
+private:
+    std::stack<int> m_stack;
+    std::stack<int> m_max_so_far;
+    
 };
     
 void Stack::Push(int x) {
-        
+    m_stack.push(x);
+    int max_so_far = 0;
+    if (!m_max_so_far.empty()) {
+        max_so_far = m_max_so_far.top();
+        if (x > max_so_far)
+            m_max_so_far.push(x);
+        else
+            m_max_so_far.push(max_so_far);
+    }
+    else {
+        m_max_so_far.push(x);
+    }
 }
     
 int Stack::Pop() {
-        return 0;
+    if (!m_stack.empty()) {
+        int x = m_stack.top();
+        m_stack.pop();
+        m_max_so_far.pop();
+    
+        return x;
+    }
 }
     
-int Stack::Max() {
-        return 0;
+const int Stack::Max() const {
+    return m_max_so_far.top();
 }
 
 // @exclude
@@ -36,11 +58,13 @@ int main(int argc, char* argv[]) {
   Stack s;
   s.Push(1);
   s.Push(2);
+  
   assert(s.Max() == 2);
   cout << s.Max() << endl;  // 2
   cout << s.Pop() << endl;  // 2
-  assert(s.Max() == 1);
+
   cout << s.Max() << endl;  // 1
+  assert(s.Max() == 1);
   s.Push(3);
   s.Push(2);
   assert(s.Max() == 3);
@@ -51,16 +75,17 @@ int main(int argc, char* argv[]) {
   s.Pop();
   assert(s.Max() == 1);
   cout << s.Max() << endl;  // 1
-  s.Pop();
+  //s.Pop();
   try {
     s.Max();
     s.Pop();
     s.Pop();
-    s.Pop();
-    s.Pop();
+    //s.Pop();
+    //s.Pop();
   }
   catch (const std::exception& e) {
     cout << e.what() << endl;
   }
+  
   return 0;
 }
